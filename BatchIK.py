694002b2457d23bc19b88aa720992dca0316c80a
Laurie Needham <ln424@bath.ac.uk>
2022-09-01 10:38:05 +0100

    Refactor trc portion of batchIK tool

--- BatchIK.py
+++ BatchIK.py
@@ -1,59 +1,99 @@
-    """batch_ik.py
+"""batch_ik.py
 
-    Tool for batch processing trc files using OpenSim's IK solver
-    """
+Tool for batch processing trc files using OpenSim's IK solver
+"""
 import os
 import time
 
-from utils.osim_utils import run_osim_tools, generate_ik_settings, generate_scale_settings
+import config
+
+from utils.maths_utils import (
+    run_osim_tools,
+    generate_ik_settings,
+    generate_scale_settings,
+)
+from utils.trc_utils import generate_trc, validate_trc, fix_trc
+
 
 def check_for_model(path, model):
     dir = os.path.dirname(path.replace("/op-fused", ""))
     return os.path.isfile(os.path.join(dir, model))
 
-# Process file older than n days
-limit_days = 4
 
-treshold = time.time() - limit_days*86400
+# # Process file older than n days
+# limit_days = 4
 
-# set data paths
-data_path = "/home/ln424/Documents/CAMERA/BioCV_Project/markerless_data/2020-02-24-P19/P19_RUN_08"
+# treshold = time.time() - limit_days * 86400
 
-# walk through data_path sub dirs and extract data
-old = []
-new = []
-for root, dirs, files in os.walk(data_path):
+# # set data paths
+# data_path = "/home/ln424/Documents/CAMERA/BioCV_Project/markerless_data/2020-02-24-P19/P19_RUN_08"
+
+# # walk through data_path sub dirs and extract data
+# old = []
+# new = []
+# for root, dirs, files in os.walk(data_path):
+#     for file in files:
+#         if file.endswith("markers.trc"):
+#             # construct full file path for this file
+#             temp_path = os.path.join(root, file)
+
+#             # skip unwanted trials such as statics and data from ap and dlc
+#             if (
+#                 "STATIC" not in temp_path
+#                 and "ap-fused" not in temp_path
+#                 and "dlc-fused" not in temp_path
+#             ):
+#                 # check file was not generated in the last n days
+#                 creation_time = os.stat(temp_path.replace(".trc", ".mot")).st_ctime
+#                 if creation_time > treshold:
+#                     old.append(root)
+#                     continue
+#                 else:
+#                     new.append(root)
+
+#                 if file.endswith("markers.trc"):
+#                     # check if we have a scaled marker-based model for this participant
+#                     if not check_for_model(root, "biocv_fullbody_markers_scaled.osim"):
+#                         # create a new scaled model for the
+#                         # ik solver to use for this participant
+#                         print(f"\n[INFO] - Scaling markers: {temp_path}\n")
+#                         run_osim_tools(root, job="scale")
+
+#                 elif file.endswith("joints_kalman.trc"):
+#                     # check if we have a scaled markerless model for this participant
+#                     if not check_for_model(
+#                         root, "biocv_fullbody_markerless_scaled.osim"
+#                     ):
+#                         # create a new scaled model for the
+#                         # ik solver to use for this participant
+#                         print(f"\n[INFO] - Scaling markerless: {temp_path}\n")
+#                         run_osim_tools(root, job="scale")
+
+
+for root, dirs, files in os.walk(config.PATH):
     for file in files:
-        if file.endswith("markers.trc"):
+        # If we need to generate trc files then we need to search
+        # for c3ds
+        if config.CONVERT_C3D and file.endswith(".c3d"):
             # construct full file path for this file
             temp_path = os.path.join(root, file)
             
-            # skip unwanted trials such as statics and data from ap and dlc
-            if "STATIC" not in temp_path and "ap-fused" not in temp_path and "dlc-fused" not in temp_path:
-                # check file was not generated in the last n days
-                creation_time = os.stat(temp_path.replace(".trc", ".mot")).st_ctime
-                if creation_time > treshold:
-                    old.append(root)
-                    continue
-                else:
-                    new.append(root)
-
-                if file.endswith("markers.trc"):
-                    # check if we have a scaled marker-based model for this participant
-                    if not check_for_model(root, "biocv_fullbody_markers_scaled.osim"):
-                        # create a new scaled model for the 
-                        # ik solver to use for this participant
-                        print(f"\n[INFO] - Scaling markers: {temp_path}\n")
-                        run_osim_tools(root, job="scale")
-
-                elif file.endswith("joints_kalman.trc"):
-                    # check if we have a scaled markerless model for this participant
-                    if not check_for_model(root, "biocv_fullbody_markerless_scaled.osim"):
-                        # create a new scaled model for the 
-                        # ik solver to use for this participant
-                        print(f"\n[INFO] - Scaling markerless: {temp_path}\n")
-                        run_osim_tools(root, job="scale")
-
-
-
-
+            # generate trc file using config settings
+            generate_trc(temp_path, rotate=[config.ROTATE_X, config.ROTATE_Y, config.ROTATE_Z])
+            
+            if config.VALIDATE_TRC:
+                # check that resulting trc file is readable
+                try:
+                    trc_savepath = temp_path.replace(".c3d", ".trc")
+                    validate_trc(trc_savepath)
+                except Exception as e:
+                    print(f"[WARNING] - {trc_savepath} not readable - attempting to replace nans")
+                    # read file in as csv and replace nans
+                    fix_trc(trc_savepath)
+                    # check that we have fixed the nan/0 issue
+                    try:
+                        validate_trc(trc_savepath)
+                        print(f"[INFO] - Warning resolved")
+                    except Exception as e:
+                        print(f"[WARNING] - {trc_savepath} still not readable")
+                        print(f"[WARNING] - {e}")
--- config.py
+++ config.py
@@ -3,16 +3,75 @@
 
 """
 
+#
+# GENERAL SETTINGS
+#
+
+# Set path to directory containing data. Data can seperated into 
+# futher subdirectories as all subdirs will be searched recursively.
 PATH = "/test/test/test/"
 
+# Are we working with markerless data i.e. COCO keypoint outputs? If
+# so, set to True. Otherwise if using BioCV markerset based data set
+# to False. Other models will require custom paths settings
+IS_MARKERLESS = True
+
+# It is assumed that default .osim, scale settings (.xml) and IK settings
+# (.xml) are stored here for both marker and markerless data. Defaults
+# will be used unless and custom path is supplied.
+DEFAULT_MODELS_PATH = "./configs/"
+
+# Use to set a path to a custom model or custom scaling/IK settings. To 
+# use default option, ensure that custom path is set to None.
+CUSTOM_MODEL_PATH = None
+CUSTOM_SCALE_SETTINGS_PATH = None
+CUSTOM_IK_SETTINGS_PATH = None
+
+#
+# TRC GENERATION SETTINGS
+#
+
+# Set to True if we want to search for .c3d files and convert them
+# to an OpenSim .trc format. If set to false we will assume that
+# .trc files already exist and search for those instead of .c3ds.
 CONVERT_C3D = True
 
+# We can attempt to validate the .trc files that we create and if
+# they fail, attempt to fix them. However, this will increase 
+# processing time.
 VALIDATE_TRC = True
 
-ROTATE_LCS = True
-
+# During .trc generation we can apply a rotation to the coordinate
+# global coordinate system if needed. OpenSim expects the y-axis to
+# be normal to the x-z ground plane. If no rotation is needed set 
+# each rotation var to 0.
+# The rotation in degree to be applied to each axis.
 ROTATE_X = -90
 ROTATE_Y = 0
 ROTATE_Z = -90
 
+#
+# SCALING SETTINGS
+#
+
+#
+SCALE_MODEL = True
+
+#
+STATIC_NAME = "STATIC"
+
+#
+SCALE_TIME_RANGE = [0, 1]
+
+
+#
+# IK SETTINGS
+#
+
+
+# Set to run the IK solver
 RUN_IK = True
+
+IK_TIME_RANGE = [0, 100]
+IK_REPORT_ERRORS = False
+IK_REPORT_MARKER_LOCATIONS = False
