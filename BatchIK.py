280916ae9cb313e518e153f618bc99f7012020c6
Laurie Needham <ln424@bath.ac.uk>
2022-09-01 18:25:47 +0100

    Refactor batchIK to handle marker and markerless data

--- BatchIK.py
+++ BatchIK.py
@@ -3,97 +3,32 @@
 Tool for batch processing trc files using OpenSim's IK solver
 """
 import os
-import time
-
 import config
-
-from utils.maths_utils import (
-    run_osim_tools,
-    generate_ik_settings,
-    generate_scale_settings,
-)
-from utils.trc_utils import generate_trc, validate_trc, fix_trc
+from utils.osim_utils import run_osim_tools
 
 
-def check_for_model(path, model):
-    dir = os.path.dirname(path.replace("/op-fused", ""))
-    return os.path.isfile(os.path.join(dir, model))
-
-
-# # Process file older than n days
-# limit_days = 4
-
-# treshold = time.time() - limit_days * 86400
-
-# # set data paths
-# data_path = "/home/ln424/Documents/CAMERA/BioCV_Project/markerless_data/2020-02-24-P19/P19_RUN_08"
-
-# # walk through data_path sub dirs and extract data
-# old = []
-# new = []
-# for root, dirs, files in os.walk(data_path):
-#     for file in files:
-#         if file.endswith("markers.trc"):
-#             # construct full file path for this file
-#             temp_path = os.path.join(root, file)
-
-#             # skip unwanted trials such as statics and data from ap and dlc
-#             if (
-#                 "STATIC" not in temp_path
-#                 and "ap-fused" not in temp_path
-#                 and "dlc-fused" not in temp_path
-#             ):
-#                 # check file was not generated in the last n days
-#                 creation_time = os.stat(temp_path.replace(".trc", ".mot")).st_ctime
-#                 if creation_time > treshold:
-#                     old.append(root)
-#                     continue
-#                 else:
-#                     new.append(root)
-
-#                 if file.endswith("markers.trc"):
-#                     # check if we have a scaled marker-based model for this participant
-#                     if not check_for_model(root, "biocv_fullbody_markers_scaled.osim"):
-#                         # create a new scaled model for the
-#                         # ik solver to use for this participant
-#                         print(f"\n[INFO] - Scaling markers: {temp_path}\n")
-#                         run_osim_tools(root, job="scale")
-
-#                 elif file.endswith("joints_kalman.trc"):
-#                     # check if we have a scaled markerless model for this participant
-#                     if not check_for_model(
-#                         root, "biocv_fullbody_markerless_scaled.osim"
-#                     ):
-#                         # create a new scaled model for the
-#                         # ik solver to use for this participant
-#                         print(f"\n[INFO] - Scaling markerless: {temp_path}\n")
-#                         run_osim_tools(root, job="scale")
-
-
+# First we search for static file that need scaling. We do this first
+# as we will need our scaled models to run IK on other motion trials
 for root, dirs, files in os.walk(config.PATH):
     for file in files:
-        # If we need to generate trc files then we need to search
-        # for c3ds
-        if config.CONVERT_C3D and file.endswith(".c3d"):
+        # If we find a static file we can scale it if the config
+        # file specifies this
+        if config.SCALE_MODEL and config.STATIC_NAME in file and file.endswith(".trc"):
             # construct full file path for this file
             temp_path = os.path.join(root, file)
             
-            # generate trc file using config settings
-            generate_trc(temp_path, rotate=[config.ROTATE_X, config.ROTATE_Y, config.ROTATE_Z])
+            print(f"\n[INFO] - Scaling model: {temp_path}\n")
+            run_osim_tools(temp_path, config, job="scale")
+
+# Once we have scaled any statics we can now run the IK solver
+# on any motion files that we find
+for root, dirs, files in os.walk(config.PATH):
+    for file in files:
+        # If we find a static file we can scale it if the config
+        # file specifies this
+        if config.RUN_IK and config.STATIC_NAME not in file and file.endswith(".trc"):
+            # construct full file path for this file
+            temp_path = os.path.join(root, file)
             
-            if config.VALIDATE_TRC:
-                # check that resulting trc file is readable
-                try:
-                    trc_savepath = temp_path.replace(".c3d", ".trc")
-                    validate_trc(trc_savepath)
-                except Exception as e:
-                    print(f"[WARNING] - {trc_savepath} not readable - attempting to replace nans")
-                    # read file in as csv and replace nans
-                    fix_trc(trc_savepath)
-                    # check that we have fixed the nan/0 issue
-                    try:
-                        validate_trc(trc_savepath)
-                        print(f"[INFO] - Warning resolved")
-                    except Exception as e:
-                        print(f"[WARNING] - {trc_savepath} still not readable")
-                        print(f"[WARNING] - {e}")
+            print(f"\n[INFO] - Running IK solver: {temp_path}\n")
+            run_osim_tools(temp_path, config, job="ik")
--- utils/osim_utils.py
+++ utils/osim_utils.py
@@ -1,7 +1,6 @@
 import os
 import lxml.etree as ET
 import subprocess
-import glob
 
 def _get_root(path):
     return os.path.dirname(os.path.dirname(path.replace("/op-fused", "")))
@@ -18,6 +17,18 @@ def _update_current_part(path):
     idx = path.rfind("P")
     return path[idx:idx+3]
 
+def _check_for_model(path, model):
+    dir = os.path.dirname(path.replace("/op-fused", ""))
+    return os.path.isfile(os.path.join(dir, model))
+
+def _find_scaled_model(base_dir: str, ext: str, substr: str) -> str:
+    for root, dirs, files in os.walk(base_dir):
+            for file in files:
+                if substr in file and file.endswith(ext):
+                    return os.path.join(root, file)
+    
+    raise FileNotFoundError(f"[Error] - Could not locate: {base_dir}")
+
 def generate_ik_settings(path: str, config: object) -> str:
 
     # generate paths for xml file
@@ -28,19 +39,24 @@ def generate_ik_settings(path: str, config: object) -> str:
     # motion trial. However, there could be several if a number of static
     # trials have been collected.
     if config.IS_MARKERLESS:
-        model_file = os.path.join(config.DEFAULT_MODELS_PATH, "biocv_fullbody_markerless_scaled.osim")
+        # To find a markerless model we'll assume that the static and scaled osim
+        # files are stored in the same parent dir as this trial.
+        model_file = _find_scaled_model(os.path.dirname(os.path.dirname(path)), "biocv_fullbody_markerless_scaled.osim", config.STATIC_NAME)
+        
     else:
         # if we are dealing with marker based data i.e. from QTM
         # we'll assume (for now) that there is a static model in the
         # dir as the one that out current file resides in.
-        model_file = [os.path.join(path, f) for f in os.listdir(os.path.dirname(path)) if config.STATIC_NAME in f and f.endswith("biocv_fullbody_markerless_scaled.osim")][0]
-        if not model_file:
-            raise FileNotFoundError()
+        model_file = _find_scaled_model(os.path.dirname(os.path.dirname(path)), "biocv_fullbody_markers_scaled.osim", config.STATIC_NAME)
+        
     # load generic ik setting
     if config.IS_MARKERLESS:
         xml_path = os.path.join(config.DEFAULT_MODELS_PATH, "ik_markerless.xml")
+        xml_savepath = marker_file.replace(".trc", "_ik_markerless.xml")
     else:
         xml_path = os.path.join(config.DEFAULT_MODELS_PATH, "ik_markers.xml")
+        xml_savepath = marker_file.replace(".trc", "_ik_markers.xml")
+        
     tree = ET.parse(xml_path)
     
     # update xml settings for the current trial
@@ -51,9 +67,10 @@ def generate_ik_settings(path: str, config: object) -> str:
         elem.text = output_motion_file
     for elem in xml_root.iter('model_file'):
         elem.text = model_file
-    
+    for elem in xml_root.iter('time_range'):
+        elem.text = str(config.IK_TIME_RANGE[0]) + " " + str(config.IK_TIME_RANGE[1])
     # write updated xml to disk
-    tree.write(xml_path)
+    tree.write(xml_savepath)
     
     return xml_path
 
@@ -73,8 +90,11 @@ def generate_scale_settings(path: str, config: object) -> str:
     # load generic scaling settings
     if config.IS_MARKERLESS:
         xml_path = os.path.join(config.DEFAULT_MODELS_PATH, "scale_op.xml")
+        xml_savepath = trc_file.replace(".trc", "_scale_op.xml")
     else:
         xml_path = os.path.join(config.DEFAULT_MODELS_PATH, "scale_markers.xml")
+        xml_savepath = trc_file.replace(".trc", "_scale_markers.xml")
+
     tree = ET.parse(xml_path)
 
     # update xml settings for the current trial
@@ -87,11 +107,13 @@ def generate_scale_settings(path: str, config: object) -> str:
         elem.text = model_file
     for elem in tree.xpath("//ScaleTool"):
         elem.attrib['name'] = model_name
+    for elem in xml_root.iter('time_range'):
+        elem.text = str(config.SCALE_TIME_RANGE[0]) + " " + str(config.SCALE_TIME_RANGE[1])
 
     # write updated xml to disk
-    tree.write(xml_path)
+    tree.write(xml_savepath)
     
-    return xml_path
+    return xml_savepath
 
 def run_osim_tools(path: str, config: object, job="ik") -> None:
     if job == "ik":
